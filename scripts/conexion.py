4d9277dcd331db333af37d59549098f9a24b7c3e
arley02 <80132545+arley02@users.noreply.github.com>
2024-04-15 22:01:25 +0200

    Add files via upload

---
+++ scripts/conexion.py
@@ -0,0 +1,41 @@
+import psycopg2
+def login(credenciales):
+#demanar usuari i contrasenya per la conexio
+    username = input("Introduïu el nom d'usuari: ")
+    password = input("Introduïu la contrasenya: ")
+    if username in credenciales:
+
+#per ara la connexió es realitza amb un usuari amb permisos de la base de dades, Després es configura perquè els usuaris corresponents puguin llogar-se a la base de dades amb els rols que li toquen.
+#en cas de tenir dades diferents com la IP o l'usuari i contrasenya canviar-la per assegurar la connexió
+    #proba de conexio i de visualitzacio de dades.
+        connexio = psycopg2.connect(host="192.168.56.112", dbname="HR",  user="postgres", password="Blanes2121")
+        cur = connexio.cursor()
+        cur.execute("SELECT * FROM departments;")  
+        reg = cur.fetchone()
+        lineas = len(("| {:<2} | {:<25} | {:<8} |".format(reg[0], reg[1], reg[2])))
+        print("-"*lineas) 
+        print("| {:<2} | {:<25} | {:<8} |".format("ID", "Departamento", "ID_Local"))
+        print("-"*lineas) 
+
+        while reg is not None: 
+            print("| {:<2} | {:<25} | {:<8} |".format(reg[0], reg[1], reg[2]));  
+            reg = cur.fetchone()
+            print("-"*lineas)
+    else:
+        print("usuari o contraseña ")
+
+#registre del usuari, falta configurar per asignar els rols
+def registre(new_user, new_password):
+    creacio_usuari = f"CREATE USER {new_user} WITH PASSWORD '{new_password}';"
+
+    try:
+        connexio = psycopg2.connect(host="192.168.56.112", dbname="HR", user="postgres", password="Blanes2121")
+        cur = connexio.cursor()
+        cur.execute(creacio_usuari)
+        connexio.commit()
+
+        cur.close()
+        connexio.close()
+
+    except Exception as e:
+        print("Error en intentar crear l'usuari:", e)
