b19917572d6b2faa9bc2036d61f579d6b1222fac
Dmitrii-F4rgus <123185694+Dmitrii-F4rgus@users.noreply.github.com>
2024-05-10 15:15:26 +0200

    Update SQLhospital_new.sql

--- ER-Relacional-SQL/SQLhospital_new.sql
+++ ER-Relacional-SQL/SQLhospital_new.sql
@@ -27,58 +27,65 @@ CREATE TABLE PLANTA (
 
 CREATE TABLE PACIENTE (
     dni_paciente VARCHAR(20) PRIMARY KEY,
-    nombre VARCHAR(50),
-    apellidos VARCHAR(100)
+    nombre VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE EQUIPA (
-    id_equipamiento SERIAL PRIMARY KEY
+    id_equipamiento SERIAL PRIMARY KEY,
+    nombre_equipo VARCHAR(100) NOT NULL,
+    marca VARCHAR(50),
+    modelo VARCHAR(50),
+    año_fabricacion INT,
+    estado VARCHAR(20),
+    ubicacion_actual VARCHAR(100)
 );
+
 
 CREATE TABLE HABITACION (
     numero_habitacion SERIAL PRIMARY KEY,
-    numero_planta INT,
-    id_equipamiento INT,
+    numero_planta INT NOT NULL,
+    id_equipamiento INT NOT NULL,
     FOREIGN KEY (numero_planta) REFERENCES PLANTA(numero_planta),
     FOREIGN KEY (id_equipamiento) REFERENCES EQUIPA(id_equipamiento)
 );
 
 CREATE TABLE QUIROFANO (
-    id_quirofano SERIAL PRIMARY KEY,
-    numero_planta INT,
-    id_equipamiento INT,
+    id_quirofano VARCHAR(10) PRIMARY KEY,
+    numero_planta INT NOT NULL,
+    id_equipamiento INT NOT NULL,
     FOREIGN KEY (numero_planta) REFERENCES PLANTA(numero_planta),
     FOREIGN KEY (id_equipamiento) REFERENCES EQUIPA(id_equipamiento)
 );
 
 CREATE TABLE PERSONAL (
     id_personal SERIAL PRIMARY KEY,
-    nombre VARCHAR(20),
-    apellidos VARCHAR(30),
-    dni VARCHAR(30)
+    nombre VARCHAR(20) NOT NULL,
+    apellidos VARCHAR(30) NOT NULL,
+    dni VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE MEDICO (
     id_medico SERIAL PRIMARY KEY,
-    id_personal INT UNIQUE,
-    estudios VARCHAR(20),
-    especialidad VARCHAR(30),
-    curriculum VARCHAR(30),
+    id_personal INT UNIQUE NOT NULL,
+    estudios VARCHAR(50) NOT NULL,
+    especialidad VARCHAR(70),
+    curriculum VARCHAR(70),
     FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal)
 );
 
 CREATE TABLE VARIO (
     id_vario SERIAL PRIMARY KEY,
-    id_personal INT UNIQUE,
+    id_personal INT UNIQUE NOT NULL,
     tipo VARCHAR(30),
     FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal)
 );
 
 CREATE TABLE ENFERMERA (
     id_enfermera SERIAL PRIMARY KEY,
-    id_personal INT UNIQUE,
+    id_personal INT UNIQUE NOT NULL,
     id_medico INT UNIQUE,
-    anos_de_experiencia INT,
+    anos_de_experiencia INT CHECK (anos_de_experiencia >= 0),
     numero_planta INT,
     FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal),
     FOREIGN KEY (id_medico) REFERENCES MEDICO(id_medico),
@@ -87,20 +94,20 @@ CREATE TABLE ENFERMERA (
 
 CREATE TABLE RES_VIS (
     id_visita SERIAL PRIMARY KEY,
-    id_personal INT UNIQUE,
-    dni_paciente VARCHAR(20) UNIQUE,
+    id_personal INT NOT NULL,
+    dni_paciente VARCHAR(20) NOT NULL,
     FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal),
     FOREIGN KEY (dni_paciente) REFERENCES PACIENTE(dni_paciente)
 );
 
 CREATE TABLE HISTORIAL_MEDICO (
     id_historial SERIAL PRIMARY KEY,
-    id_visita INT,
-    id_personal_doctor INT,
+    id_visita INT NOT NULL,
+    id_personal_doctor INT NOT NULL,
     codigo_diagnostico INT,
-	fecha VARCHAR(20),
-    dni_paciente VARCHAR(20),
-	medicamentos_recibidos TEXT,
+    fecha VARCHAR(20) NOT NULL,
+    dni_paciente VARCHAR(20) NOT NULL,
+    medicamentos_recibidos TEXT,
     FOREIGN KEY (id_personal_doctor) REFERENCES PERSONAL(id_personal),
     FOREIGN KEY (codigo_diagnostico) REFERENCES DIAGNOSTICO(codigo_diagnostico),
     FOREIGN KEY (dni_paciente) REFERENCES PACIENTE(dni_paciente),
@@ -108,18 +115,83 @@ CREATE TABLE HISTORIAL_MEDICO (
 );
 
 CREATE TABLE HABITACION_VISITADA (
-    id_visita INT,
-    id_habitacion INT,
+    id_visita INT NOT NULL,
+    id_habitacion INT NOT NULL,
     PRIMARY KEY (id_visita, id_habitacion),
     FOREIGN KEY (id_visita) REFERENCES RES_VIS(id_visita),
     FOREIGN KEY (id_habitacion) REFERENCES HABITACION(numero_habitacion)
 );
 
-
 CREATE TABLE QUIROFANO_VISITADA (
-    id_visita INT,
-    id_quirofano INT,
+    id_visita INT NOT NULL,
+    id_quirofano VARCHAR(10) NOT NULL,
     PRIMARY KEY (id_visita, id_quirofano),
     FOREIGN KEY (id_visita) REFERENCES RES_VIS(id_visita),
     FOREIGN KEY (id_quirofano) REFERENCES QUIROFANO(id_quirofano)
 );
+
+-- Добавление внешнего ключа к таблице HABITACION
+ALTER TABLE HABITACION
+ADD CONSTRAINT fk_numero_planta FOREIGN KEY (numero_planta) REFERENCES PLANTA(numero_planta);
+
+ALTER TABLE HABITACION
+ADD CONSTRAINT fk_id_equipamiento FOREIGN KEY (id_equipamiento) REFERENCES EQUIPA(id_equipamiento);
+
+-- Добавление внешнего ключа к таблице QUIROFANO
+ALTER TABLE QUIROFANO
+ADD CONSTRAINT fk_numero_planta_quirofano FOREIGN KEY (numero_planta) REFERENCES PLANTA(numero_planta);
+
+ALTER TABLE QUIROFANO
+ADD CONSTRAINT fk_id_equipamiento_quirofano FOREIGN KEY (id_equipamiento) REFERENCES EQUIPA(id_equipamiento);
+
+-- Добавление внешнего ключа к таблице MEDICO
+ALTER TABLE MEDICO
+ADD CONSTRAINT fk_id_personal_medico FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal);
+
+-- Добавление внешнего ключа к таблице VARIO
+ALTER TABLE VARIO
+ADD CONSTRAINT fk_id_personal_vario FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal);
+
+-- Добавление внешнего ключа к таблице ENFERMERA
+ALTER TABLE ENFERMERA
+ADD CONSTRAINT fk_id_personal_enfermera FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal);
+
+ALTER TABLE ENFERMERA
+ADD CONSTRAINT fk_id_medico_enfermera FOREIGN KEY (id_medico) REFERENCES MEDICO(id_medico);
+
+ALTER TABLE ENFERMERA
+ADD CONSTRAINT fk_numero_planta_enfermera FOREIGN KEY (numero_planta) REFERENCES PLANTA(numero_planta);
+
+-- Добавление внешнего ключа к таблице RES_VIS
+ALTER TABLE RES_VIS
+ADD CONSTRAINT fk_id_personal_res_vis FOREIGN KEY (id_personal) REFERENCES PERSONAL(id_personal);
+
+ALTER TABLE RES_VIS
+ADD CONSTRAINT fk_dni_paciente_res_vis FOREIGN KEY (dni_paciente) REFERENCES PACIENTE(dni_paciente);
+
+-- Добавление внешнего ключа к таблице HISTORIAL_MEDICO
+ALTER TABLE HISTORIAL_MEDICO
+ADD CONSTRAINT fk_id_visita_historial FOREIGN KEY (id_visita) REFERENCES RES_VIS(id_visita);
+
+ALTER TABLE HISTORIAL_MEDICO
+ADD CONSTRAINT fk_id_personal_doctor FOREIGN KEY (id_personal_doctor) REFERENCES PERSONAL(id_personal);
+
+ALTER TABLE HISTORIAL_MEDICO
+ADD CONSTRAINT fk_codigo_diagnostico_historial FOREIGN KEY (codigo_diagnostico) REFERENCES DIAGNOSTICO(codigo_diagnostico);
+
+ALTER TABLE HISTORIAL_MEDICO
+ADD CONSTRAINT fk_dni_paciente_historial FOREIGN KEY (dni_paciente) REFERENCES PACIENTE(dni_paciente);
+
+-- Добавление внешнего ключа к таблице HABITACION_VISITADA
+ALTER TABLE HABITACION_VISITADA
+ADD CONSTRAINT fk_id_visita_habitacion FOREIGN KEY (id_visita) REFERENCES RES_VIS(id_visita);
+
+ALTER TABLE HABITACION_VISITADA
+ADD CONSTRAINT fk_id_habitacion_habitacion FOREIGN KEY (id_habitacion) REFERENCES HABITACION(numero_habitacion);
+
+-- Добавление внешнего ключа к таблице QUIROFANO_VISITADA
+ALTER TABLE QUIROFANO_VISITADA
+ADD CONSTRAINT fk_id_visita_quirofano FOREIGN KEY (id_visita) REFERENCES RES_VIS(id_visita);
+
+ALTER TABLE QUIROFANO_VISITADA
+ADD CONSTRAINT fk_id_quirofano_quirofano FOREIGN KEY (id_quirofano) REFERENCES QUIROFANO(id_quirofano);
